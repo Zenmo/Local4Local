01fbbfd0f45225698dc15fcb333cd985b38d1160
ThyVonR <thyvonr@gmail.com>
2024-08-12 14:10:38 +0200

    Opzet generic classes (#17)
    * Opzet generic classes
    
    * Generic classes aangepast en aangevuld op basis van feedback

--- webtool/gradle/common/src/commonMain/kotlin/Pilot.kt
+++ webtool/gradle/common/src/commonMain/kotlin/Pilot.kt
@@ -7,4 +7,81 @@ import kotlin.js.JsExport
 @JsExport
 data class Pilot(
     val name: String,
+    val households: List<Households>,
+    val companies: List<Company>,
+    val solarFarms: List<SolarFarm>,
+    val windFarms: List<WindFarm>,
+    val batteries: List<Battery>,
+    val heatStorages: List<HeatStorage>,
 )
+
+@OptIn(ExperimentalJsExport::class)
+@JsExport
+data class Households(
+    val type: String,
+    val households_n: Int,
+    val hasPV_r: Double,
+    val hasHeatPump_r: Double,
+    val hasChargePoint_r: Double,
+    val hasHomeBattery_r: Double,
+    
+    /**Jaarlijks gemiddeld basisverbruik zonder warmtepomp, elektrische voertuigen en zonnepanelen */
+    val yearlyBaseConsumptionAvg_kWh: Double,
+)
+
+@OptIn(ExperimentalJsExport::class)
+@JsExport
+data class Company(
+    val name: String,
+)
+
+/*
+@OptIn(ExperimentalJsExport::class)
+@JsExport
+data class Utility(
+    val name: String,
+    val type: String,
+)
+
+@OptIn(ExperimentalJsExport::class)
+@JsExport
+data class ConsumptionAsset(
+    val consumptionType: String,
+    val consumption_kW: Double,
+    val yearlyConsumption_kWh: Double,
+)
+*/
+
+@OptIn(ExperimentalJsExport::class)
+@JsExport
+data class SolarFarm(
+    val nominalPower_kW: Double,
+)
+
+@OptIn(ExperimentalJsExport::class)
+@JsExport
+data class WindFarm(
+    val nominalPower_kW: Double,
+)
+
+@OptIn(ExperimentalJsExport::class)
+@JsExport
+data class Battery(
+    val capacity_kWh: Double,
+    val peakPower_kW: Double,
+)
+
+@OptIn(ExperimentalJsExport::class)
+@JsExport
+data class HeatStorage(
+    val storageMedium: String,
+    val storageVolume_m3: Double,
+    val minTemp_degC: Double,
+    val maxTemp_degC: Double,
+) {
+    fun getCapacity_kWh(): Double {
+        val specificHeatCapacity = 4.18 // kJ/kg/K
+
+        return storageVolume_m3 * 1000 * specificHeatCapacity * (minTemp_degC - maxTemp_degC) / 3600
+    }
+}
