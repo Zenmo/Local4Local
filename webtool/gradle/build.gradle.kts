eda888a6ed4365b42ea881e0b96fcbb39f8b35d2
Erik van Velzen <erik@evanv.nl>
2024-08-12 12:05:07 +0200

    Add backend
    Http4k, compose, gradle, etc.

---
+++ webtool/gradle/build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    id("org.jetbrains.kotlin.multiplatform") version "2.0.10" apply false
+    kotlin("jvm") version "2.0.10" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.10" apply false
+}
