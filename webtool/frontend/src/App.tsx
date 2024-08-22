4912e9ef9af96f7bfbb8d5d1a79b4bfc072fe005
Erik van Velzen <erik@evanv.nl>
2024-08-22 16:06:38 +0200

    Connect anylogic to frontend (#23)
    Frontend isn't connected to backend yet.

--- webtool/frontend/src/App.tsx
+++ webtool/frontend/src/App.tsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
+import {useState} from "react"
+import viteLogo from "/vite.svg"
 import {Pilot} from "local4local"
 import {css} from "@emotion/react"
 import {ViewPilot} from "./components/view-pilot.tsx"
+import {startSimulation} from "./components/anylogic.ts"
 
 function App() {
     const [pilot, setPilot] = useState(new Pilot("Pilot"))
+    const anylogicElementId = "anylogic"
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} css={css`
-              height: 6em;
-              padding: 1.5em;
-              will-change: filter;
-              transition: filter 300ms;
-              &:hover {
-                  filter: drop-shadow(0 0 2em #646cffaa);
-              }
-              &:hover {
-                  filter: drop-shadow(0 0 2em #61dafbaa);
-              }
-          `} alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Systeemontwerptool</h1>
-        <ViewPilot pilot={pilot} setPilot={setPilot}/>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={viteLogo} css={css`
+                        height: 6em;
+                        padding: 1.5em;
+                        will-change: filter;
+                        transition: filter 300ms;
+
+                        &:hover {
+                            filter: drop-shadow(0 0 2em #646cffaa);
+                        }
+
+                        &:hover {
+                            filter: drop-shadow(0 0 2em #61dafbaa);
+                        }
+                    `} alt="Vite logo"/>
+                </a>
+            </div>
+            <h1>Cooperatie Configurator</h1>
+            <ViewPilot pilot={pilot} setPilot={setPilot}/>
+            <button type="button" onClick={() => startSimulation(anylogicElementId)}>Start simulatie</button>
+            <div id={anylogicElementId} style={{height: "600px"}}/>
+        </>
+    )
 }
 
 export default App
---
+++ webtool/frontend/src/components/anylogic.ts
@@ -0,0 +1,25 @@
+import "https://anylogic.zenmo.com/assets/js-client-8.5.0/cloud-client.js"
+
+// see https://anylogic.help/cloud/api/js.html
+declare const CloudClient: any
+
+export const startSimulation = async (divId: string): Promise<Animation> => {
+    // API key is of user "publiek@zenmo.com"
+    const apiKey = "17e0722f-25c4-4549-85c3-d36509f5c710"
+    const modelId = "b9f2137d-b8cc-462f-85a4-fb2379f0eb39"
+
+    const cloudClient = CloudClient.create(apiKey, "https://anylogic.zenmo.com")
+    const model = await cloudClient.getModelById(modelId)
+    const latestVersion = await cloudClient.getModelVersionByNumber(model, model.modelVersions.length)
+    const inputs = cloudClient.createDefaultInputs(latestVersion)
+    inputs.setInput("Local 4 local backend url", import.meta.env.VITE_BACKEND_URL)
+    inputs.setInput("Local 4 local session", "startPilot")
+    const animation = await cloudClient.startAnimation(inputs, divId)
+    animation.setSpeed(1)
+
+    // Example how to call into AnyLogic
+    // see https://anylogic.help/cloud/api/js.html#animation-class
+    // animation.callFunction("experiment.root.API_function_setPVslider", [6])
+
+    return animation
+}
