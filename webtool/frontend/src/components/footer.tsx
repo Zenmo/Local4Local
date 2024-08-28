623490a3e2d187e46ba503cd3f95ee1b22f1210c
Erik van Velzen <erik@evanv.nl>
2024-08-28 10:40:35 +0200

    Add github link

--- webtool/frontend/src/components/footer.tsx
+++ webtool/frontend/src/components/footer.tsx
@@ -3,10 +3,19 @@ import {css} from "@emotion/react"
 export const Footer = () => (
     <div css={css`
         padding: .5rem 1rem;
-        text-align: right;
+        //text-align: right;
+        display: flex;
+        justify-content: end;
+        align-items: center;
+        gap: 1.5rem;
     `}>
-        Gemaakt door
-        &nbsp;
-        <a href="https://zenmo.com">Zenmo</a>
+        <a style={{marginBottom: "-.4rem"}} href="https://github.com/zenmo/local4local">
+            <img style={{height: "1rem"}} src="/Git-Logo-2Color.svg" alt="Broncode" />
+        </a>
+        <span>
+            Gemaakt door
+            &nbsp;
+            <a href="https://zenmo.com">Zenmo</a>
+        </span>
     </div>
 )
