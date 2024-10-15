3b90d6896e7d99390019bf05b4a03ffb73163e21
Erik van Velzen <erik@evanv.nl>
2024-10-15 12:03:52 +0200

    Add costs to default scenario

--- webtool/gradle/backend/src/main/kotlin/nu/local4local/backend/PilotController.kt
+++ webtool/gradle/backend/src/main/kotlin/nu/local4local/backend/PilotController.kt
@@ -2,7 +2,9 @@ package nu.local4local.backend
 
 import nu.local4local.common.HouseholdGroup
 import nu.local4local.common.AssetCost
+import nu.local4local.common.Battery
 import nu.local4local.common.Pilot
+import nu.local4local.common.SolarFarm
 import nu.local4local.common.WindFarm
 import org.http4k.core.Method.PUT
 import org.http4k.core.Method.GET
@@ -14,31 +16,56 @@ import org.http4k.routing.bind
 import org.http4k.routing.path
 import org.http4k.routing.routes
 import java.util.concurrent.ConcurrentHashMap
+import kotlin.Double
 
 val startPilot = Pilot(
-    name="startPilot",
+    name = "startPilot",
+    bufferPrice_eurpkWh = 0.01,
     householdGroups = listOf(
         HouseholdGroup(
             type = "Mix",
-            households_n = 100,
+            households_n = 200,
             hasPV_r = 0.5,
-            hasHeatPump_r = 0.5,
-            hasChargePoint_r = 0.5,
-            hasHomeBattery_r = 0.5,
+            hasHeatPump_r = 0.2,
+            hasChargePoint_r = 0.2,
+            hasHomeBattery_r = 0.1,
             annualBaseConsumptionAvg_kWh = 4500.0
-        )
+        ),
     ),
     windFarms = listOf(
-        WindFarm(2000.0,
-        AssetCost(LCOE_eurpkWH = 0.0,
-            CAPEX_eur = 0.0,
-            interest_r = 0.0,
-            depreciationPeriod_y = 0.0,
-            OPEX_eurpy = 0.0
+        WindFarm(
+            nominalPower_kW = 2000.0,
+            AssetCost(
+                LCOE_eurpkWH = 0.07,
+                CAPEX_eur = 0.0,
+                interest_r = 0.0,
+                depreciationPeriod_y = 0.0,
+                OPEX_eurpy = 0.0
+            ),
+        ),
+    ),
+    solarFarms = listOf(
+        SolarFarm(
+            nominalPower_kW = 2000.0,
+            AssetCost(
+                LCOE_eurpkWH = 0.04,
             )
         )
+    ),
+    batteries = listOf(
+        Battery(
+            capacity_kWh = 200.0,
+            peakPower_kW = 100.0,
+            cost = AssetCost(
+                CAPEX_eur = 100_000.0,
+                interest_r = 0.05,
+                depreciationPeriod_y = 30.0,
+                OPEX_eurpy = 1000.0,
+            )
+        ),
     )
 )
+
 
 class PilotController {
     val pilots = ConcurrentHashMap(
