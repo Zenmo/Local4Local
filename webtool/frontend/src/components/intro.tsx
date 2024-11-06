852a77c53cd6faca80e2051b58fb772d0e2bfe51
Maria Cano <macano33@gmail.com>
2024-11-06 13:57:22 +0100

    Justify text

--- webtool/frontend/src/components/intro.tsx
+++ webtool/frontend/src/components/intro.tsx
@@ -1,5 +1,5 @@
 export const Intro = () => (
-    <>
+    <div style={{textAlign: "justify"}}>
         <p>
             Welkom bij de <strong>Local 4 Local Cooperatie Configurator</strong>. De Cooperatie Configurator is een tool waarmee je je
             eigen <strong>energiegemeenschap</strong> kan nabouwen en simuleren. Je stelt in welke <strong>zonneparken</strong> en <strong>windmolens</strong> je beheert,
@@ -25,5 +25,5 @@ export const Intro = () => (
             <strong>energiebalans</strong> en <strong>duurzaamheid</strong>, als ook met een aantrekkelijk <strong>energietarief</strong> voor je leden. Probeer het zelf
             uit, het is makkelijker dan je denkt!
         </p>
-    </>
+    </div>
 );
