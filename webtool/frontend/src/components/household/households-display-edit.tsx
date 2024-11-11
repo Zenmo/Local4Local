42cdba14ef0c6bc7295442cb6a82a821aed041da
Maria Cano <macano33@gmail.com>
2024-11-11 16:31:27 +0100

    Household Edit

---
+++ webtool/frontend/src/components/household/households-display-edit.tsx
@@ -0,0 +1,37 @@
+import {FunctionComponent, useState} from "react"
+import {HouseholdGroup, Pilot} from "local4local"
+import {HouseholdDisplay} from "./household-display.tsx";
+import {HouseholdForm} from "./household-form.tsx"
+
+export const HouseholdsDisplayEdit: FunctionComponent<{
+    pilot: Pilot,
+    onChange: (pilot: Pilot) => void,
+}> = ({pilot, onChange}) => {
+    const [selectedHouseholdGroup, setSelectedHouseholdGroup] = useState<HouseholdGroup | null>(null);
+    return (
+        <>
+            {pilot.householdGroups.asJsReadonlyArrayView().map((it, i) =>
+            selectedHouseholdGroup == it ? (
+                <HouseholdForm
+                    key={"householdGroup_" + i}
+                    save={(asset: HouseholdGroup) => {
+                        onChange(pilot.replaceHouseHoldGroup(i, asset))
+                        setSelectedHouseholdGroup(null);
+                    }}
+                    hide={() => {
+                        setSelectedHouseholdGroup(null);
+                    }}
+                    initialData={selectedHouseholdGroup}
+                />
+            ) : (
+                <HouseholdDisplay
+                    key={"householdGroup_" + i}
+                    householdGroup={it}
+                    onEdit={() => { setSelectedHouseholdGroup(it)}}
+                    toDelete={() => onChange(pilot.remove(it))}
+                />
+            )
+        )}
+        </>
+    )
+}
