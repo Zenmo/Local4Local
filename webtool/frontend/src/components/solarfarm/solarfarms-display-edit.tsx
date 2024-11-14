60be16c4ad403a4089fa430690941d2ff015a2d5
Erik van Velzen <erik@evanv.nl>
2024-11-14 11:31:02 +0100

    Fix build

--- webtool/frontend/src/components/solarfarm/solarfarms-display-edit.tsx
+++ webtool/frontend/src/components/solarfarm/solarfarms-display-edit.tsx
@@ -16,7 +16,7 @@ export const SolarFarmsDisplayEdit: FunctionComponent<{
                     <SolarFarmForm
                         key={"SolarFarm_" + i}
                         save={(asset: SolarFarm) => {
-                            onChange(pilot.replaceAsset(i, asset))
+                            onChange(pilot.replaceAsset(asset, i))
                             setSelected(null);
                         }}
                         hide={() => {
