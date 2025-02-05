bb6a22404059a189d2747aff61496278bb8e6b49
Maria Cano <macano33@gmail.com>
2025-02-05 09:33:51 -0500

    change to Popover

--- webtool/frontend/src/components/label-info.tsx
+++ webtool/frontend/src/components/label-info.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, HoverCard } from "@radix-ui/themes"
+import { Text, Popover } from "@radix-ui/themes"
 import { PiInfoLight } from 'react-icons/pi';
 
 interface LabelInfoProps {
@@ -14,18 +14,18 @@ const LabelInfo: React.FC<LabelInfoProps> = ({ data }) => {
     return (
             <Text className="form-label" htmlFor={data.name}>
                 {data.title}{" "}
-                <HoverCard.Root>
-                    <HoverCard.Trigger>
+                <Popover.Root>
+                    <Popover.Trigger>
                         <span>
                             <PiInfoLight />
                         </span>
-                    </HoverCard.Trigger>
-                    <HoverCard.Content maxWidth='300px'>
+                    </Popover.Trigger>
+                    <Popover.Content maxWidth='300px'>
                         <Text>
                             {data.infoText}
                         </Text>
-                    </HoverCard.Content>
-                </HoverCard.Root>{" "}
+                    </Popover.Content>
+                </Popover.Root>{" "}
             </Text>
     );
 };
