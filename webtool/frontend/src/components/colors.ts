fcecb996fbad49faa8d3f866a015da4d6873c157
Erik van Velzen <erik@evanv.nl>
2025-05-06 13:28:11 +0200

    Add local4local color scheme

---
+++ webtool/frontend/src/components/colors.ts
@@ -0,0 +1,7 @@
+
+// colors extracted from Local4Local.nu website
+export const local4localLightBlue = "#99C5EA"
+export const local4localDarkBlue = "#02345E"
+export const local4localDarkOrange = "#DB6E1E"
+export const local4localLightOrange = "#EFB72C"
+export const local4localLightGreen = "#AED3B3"
