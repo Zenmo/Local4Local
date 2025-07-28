8a162eabeb2199003aee23bc7de03d45cab19436
Erik van Velzen <erik@evanv.nl>
2025-07-28 12:22:06 +0200

    Add button in footer to toggle dev mode
    Dev mode status can be read through React context.

---
+++ webtool/frontend/src/components/footer/GitLink.tsx
@@ -0,0 +1,7 @@
+import {FunctionComponent} from "react"
+
+export const GitLink: FunctionComponent = () => (
+    <a style={{marginBottom: "-.4rem"}} href="https://github.com/zenmo/local4local">
+        <img style={{height: "1rem"}} src="/Git-Logo-2Color.svg" alt="Broncode" />
+    </a>
+)
