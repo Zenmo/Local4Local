6af96da7ddaa387314c56cd23e22812d80f9dc57
Erik van Velzen <erik@evanv.nl>
2025-07-28 13:15:57 +0200

    More readable colors on front page

--- webtool/frontend/src/colors.ts
+++ webtool/frontend/src/colors.ts
@@ -1,3 +1,4 @@
+import Color from "color"
 
 // colors extracted from Local4Local.nu website
 export const local4localLightBlue = "#99C5EA"
@@ -5,3 +6,7 @@ export const local4localDarkBlue = "#02345E"
 export const local4localDarkOrange = "#DB6E1E"
 export const local4localLightOrange = "#EFB72C"
 export const local4localLightGreen = "#AED3B3"
+
+// extended colors in this project
+export const local4localGreen = Color(local4localLightGreen).darken(.2).hex()
+export const local4localBlue = Color(local4localLightBlue).darken(.1).hex()
